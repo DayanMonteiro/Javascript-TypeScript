b8432e8ca6260a13f386bb5040ce7cec23ad5bff
Dayan Monteiro <dmsmamb@gmail.com>
2020-11-16 13:50:54 -0300

    aula05

---
+++ Javascript-básico/aula05-variáveis.js
@@ -0,0 +1,28 @@
+// não podemos criar variáveis com palavras reservadas
+// variáveis precisam ter nomes significativos
+// não podemos começar uma variável com um número
+// não podem conter espaçõs ou traços, devemos usar camelCase
+// as variáveis são case-sensitive, ou seja, elas diferenciam letras maiusculas de minusculas
+// será valido o ultimo valor declarado, ou  seja,
+let nomeCliente = "Laryssa"
+nomeCliente = "Dayan"
+console.log(nomeCliente); // nesse caso nome cliente assimiria sua ultima declação valendo Dayan.
+
+// NÃO UTILIZE VAR, UTILIZE LET.
+
+
+let nome = 'Dayan';
+let nome2;// declarei uma variável, mas não a defini
+nome2 = 'Laryssa'; // inicializei a variável
+
+
+console.log(nome, 'nasceu em 1987.');
+console.log('Em 2014', nome, 'conheceu Laryssa');
+console.log(nome, 'casou-se com Laryssa em 2019.');
+console.log('Laryssa teve uma filha com', nome, 'em 2020.');
+console.log('A filha de', nome,  'se chama Pietra');
+
+
+
+
+
