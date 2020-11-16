edf965cc35f5f69093607c04c99bfd588a4c161d
Dayan Monteiro <dmsmamb@gmail.com>
2020-11-16 12:48:32 -0300

    aula03

---
+++ Javascript-básico/aula02.js
@@ -0,0 +1,2 @@
+console.log('Meu nome é "Dayan". Estou aprendendo JavaScript às', 11, 'da manhã.');
+console.log('Este', 'é', 'um número', 100);
---
+++ Javascript-básico/aula03-comentários.js
@@ -0,0 +1,11 @@
+// escrever comentário
+console.log("Olá, mundo!"); // comentários de 2 barras
+console.log("Linha 01");
+console.log("Linha 02");
+console.log("Linha 03");
+/* comentários de várias linhas
+ comentários de várias linhas
+ comentários de várias linhas
+ comentários de várias linhas
+ comentários de várias linhas*/
+console.log("mais linha");
