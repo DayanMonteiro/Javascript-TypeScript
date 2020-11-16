664bbd0d5ee2a2f8948f2bb9eddfb0fa264bbc42
Dayan Monteiro <dmsmamb@gmail.com>
2020-11-16 11:59:36 -0300

    aula01

---
+++ Javascript-básico/index.js
@@ -0,0 +1,15 @@
+// strings
+console.log("teste de script");
+console.log("Dayan Monteiro");
+console.log("em breve um dev");
+console.log("para executar um texto com aspas abro com aspas duplas e coloco 'aspas simples' na parte de interesse");
+console.log('também posso abrir com aspas simples e colocar "aspas duplas" na parte de interesse');
+console.log(`ou posso colocar entre crase e usar tanto "aspas duplas" quando 'aspas simples' no meu texto`);
+
+// number
+console.log(123456);
+console.log(33.8);
+console.log(33, 24.3, 1095, "vários números");
+
+
+
