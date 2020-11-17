ddc140b3e7027a1fe5effa3c3b803a73c89b723b
Dayan Monteiro <dmsmamb@gmail.com>
2020-11-17 15:00:30 -0300

    aula11

---
+++ Javascript-básico/aula11-alert-confirm-prompt.js
@@ -0,0 +1,24 @@
+/*
+alert('Mensagem de alerta');
+confirm("deseja continuar");
+*/
+
+/* possibilidade  01 
+let num1 = prompt('Digite um número para a soma') ;
+let num2 = prompt('Digite segundo número para a soma');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const resultado = num1 + num2;
+alert(`O resultado da sua conta foi: ${resultado}`);
+*/
+
+// possibilidade 02
+let num3 = prompt('Digite um número para a soma') ;
+let num4 = prompt('Digite segundo número para a soma');
+
+num3 = Number(num3);
+num4 = Number(num4);
+
+alert(`O resultado da sua conta foi: ${num3 + num4}`);
