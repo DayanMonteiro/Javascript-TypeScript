e0feafc616e9b2d6d2f9d489c01790aa9f56bf40
Dayan Monteiro <dmsmamb@gmail.com>
2020-11-23 14:42:37 -0300

    closure att

--- JS-v/clousure.js
+++ JS-v/clousure.js
@@ -1,3 +1,5 @@
+// ex 01
+
 function init() {
     const exemplo = 'Essa variável';
 
@@ -21,6 +23,33 @@ const initFn2 = initFn1();
 const initFn3 = initFn2();
 
 initFn3();
+
+/*
+1 - o valor da variável exemplo é: Essa variável
+2 - o valor da variável é: Essa variável
+3 - o valor da variável é: Essa variável
+*/
+
+
+// ex 02
+
+function init() {
+    const exemplo = 'Essa variável';
+
+    return function() {
+        console.log(`1 - o valor da variável exemplo é: ${exemplo}`);
+
+        return function() {
+            console.log(`2 - o valor da variável é: ${exemplo}`);
+
+            return function() {
+                console.log(`3 - o valor da variável é: ${exemplo}`);
+            }
+        }
+    }
+}
+
+init() () () (); // busca de uma forma mais clean
 
 /*
 1 - o valor da variável exemplo é: Essa variável
