e40208cbe87d950382c16b2f20a398b7197b7eec
Dayan Monteiro <dmsmamb@gmail.com>
2020-11-24 13:17:41 -0300

    var01

---
+++ JS-v/variaveis-ex01.js
@@ -0,0 +1,13 @@
+var nameVar = 'Dayan';
+let nameLet = 'Dayan';
+const nameConst = 'Dayan';
+
+console.log(`nameVar: ${nameVar}`);
+console.log(`nameLet: ${nameLet}`);
+console.log(`nameConst: ${nameConst}`);
+
+/*
+nameVar: Dayan
+nameLet: Dayan
+nameConst: Dayan
+*/
