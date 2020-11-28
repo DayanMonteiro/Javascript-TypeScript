3d8ae1fe67fe26f27170bb81cb3f2f32db9efb23
Dayan Montiro <dmsmamb@gmail.com>
2020-11-28 16:57:40 -0300

    filter00

---
+++ JS-v/filter.js
@@ -0,0 +1,29 @@
+const pets = [ {
+    name: 'Thor',
+    type: 'dog' ,
+    age: 5,
+
+}, {
+
+    name: 'Opa' ,
+    type: 'cat',
+    age: 2,
+
+}, {
+    name: 'Bobão',
+    type: 'fish' ,
+    age: 1,
+
+}
+]
+console.log(pets)
+
+/*
+[
+  { name: 'Thor', type: 'cão', age: 5 },
+  { name: 'Opa', type: 'gato', age: 2 },
+  { name: 'Bobão', type: 'peixe', age: 1 }
+]
+
+
+*/
