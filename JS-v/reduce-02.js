3f34d21bbe30c2e745d8be4b5ff50115fc24a892
Dayan Monteiro <dmsmamb@gmail.com>
2020-11-30 10:20:46 -0300

    reduce ex3

---
+++ JS-v/reduce-02.js
@@ -0,0 +1,57 @@
+const pets = [ {
+    name: 'Thor',
+    type: 'dog' ,
+    age: 10,
+    weigth: 18, 
+
+}, {
+
+    name: 'Opa' ,
+    type: 'cat',
+    age: 2,
+    weigth: 18, 
+}, {
+
+    name: 'Blublu',
+    type: 'fish' ,
+    age: 1,
+    weigth: 0.02, 
+}, {
+    
+    name: 'Branquelo',
+    type: 'cat' ,
+    age: 8,
+    weigth: 4, 
+}, {
+    
+    name: 'Tobias',
+    type: 'dog' ,
+    age: 6,
+    weigth: 10, 
+}, {
+    
+    name: 'Gironda',
+    type: 'fish' ,
+    age: 2,
+    weigth: 0.01, 
+}, {
+    
+    name: 'Oponga',
+    type: 'horse' ,
+    age: 1,
+    weigth: 380, 
+}
+]
+
+/*O reduce reduz varios valores em apenas um, no exemplo pegaremos
+o peso de todos os animais juntos */
+
+const totalWeitght = pets.reduce((total, pet) => {
+    return total + pet.weigth
+}, 0)
+
+console.log(totalWeitght) 
+
+// 430.03 
+
+
