eebc36107c3b7dc30006014a04e7cde2652e205f
Dayan Monteiro <dmsmamb@gmail.com>
2020-11-30 16:51:17 -0300

    enhaced

---
+++ JS-v/enhanced-object-literals.js
@@ -0,0 +1,14 @@
+// ex 00
+var obj = {
+    prop1: "Digital"
+};
+console.log(obj); // { prop1: 'Digital' }
+
+// ex 01
+var prop2 = "CPF"
+var obj = {
+    prop2: prop2
+};
+console.log(prop2); // cpf
+
+// 0:56
