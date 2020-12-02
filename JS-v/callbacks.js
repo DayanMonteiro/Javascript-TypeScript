c4285d1f2cb2be3125ba0312c0c080851cf75c9f
Dayan Montiro <dmsmamb@gmail.com>
2020-12-01 21:25:21 -0300

    callback 00

---
+++ JS-v/callbacks.js
@@ -0,0 +1,41 @@
+function doSomething(callback) {
+    setTimeout(function(){
+        // did something
+        callback('First data');
+    }, 1000); 
+}
+
+function doOtherThing(callback) {
+    setTimeout(function() {
+        // did other thing
+        callback('Second data');
+    }, 1000);
+}
+
+function doAll() {
+    doSomething(function(data) {
+        var processedData = data.split('');
+
+        doOtherThing(function(data2) {
+            var processedData2 = data2.split('');
+
+                setTimeout(function() {
+                    console.log(processedData, processedData2);
+                }, 1000);
+        });
+    });
+}
+
+doAll()
+
+/*
+[
+  'F', 'i', 'r', 's',
+  't', ' ', 'd', 'a',
+  't', 'a'
+] [
+  'S', 'e', 'c', 'o',
+  'n', 'd', ' ', 'd',
+  'a', 't', 'a'
+]
+*/
