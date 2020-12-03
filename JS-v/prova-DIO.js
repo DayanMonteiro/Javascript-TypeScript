988163d3ccfc13e231a7547c25a1513130bd73a4
Dayan Monteiro <dmsmamb@gmail.com>
2020-12-03 20:12:15 -0300

    empacado no teste

--- JS-v/prova-DIO.js
+++ JS-v/prova-DIO.js
@@ -1,4 +1,3 @@
-/*
 
 var numeros = [7, -5, 6, -3.4, 4.6, 12];
 
@@ -10,7 +9,7 @@ var resultado = numeros.filter(numerosPositivos);
 console.log(resultado.length + ' valores positivos'); // 4 valores positivos
 
 ///////////////////////////////////////////////////////////
-
+/*
 let limit = parseInt(gets());
 for (let i = 0; i < limit; i++) {
     let line = gets().split(" ");
@@ -30,7 +29,7 @@ for (let i = 0; i < limit; i++) {
 
 
 /////
-
+/*
 var cont =1;
 while (cont<=100) {
     var resto = cont%2;
@@ -40,4 +39,25 @@ while (cont<=100) {
     cont = cont + 1;
 }
 console.log(Par);
-   
+*/
+ /*  
+var N = 6;
+var cont = 1;
+while(cont > 0 && cont <= N) {
+    var resto = cont%2;
+    if (resto == 0) {
+        console.log("Pares "+ cont);  
+    };
+    cont = cont + 1;
+}; */
+/*
+var N = 6;
+var cont = 1;
+for(var i = 0; i<6;i++){
+    numeros.push(parseInt(gets()));
+}*/
+let numeros = [ ];
+for(var i = 0; i<6;i++){
+    numeros.push(gets());
+}
+
