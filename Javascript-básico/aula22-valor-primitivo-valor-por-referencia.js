03599820e33d576e22b4990d6a5ce8c27b41819c
Dayan Monteiro <dmsmamb@gmail.com>
2020-12-17 17:58:35 -0300

    valores

---
+++ Javascript-básico/aula22-valor-primitivo-valor-por-referencia.js
@@ -0,0 +1,36 @@
+/*
+Primitivos (imutáveis) -  string, number, boolean, undefined, null (bigint, symbol)
+
+Referência (mutável) - array, object, function
+*/
+
+// ex00     12345
+let nome = 'Dayan';
+ console.log(nome[0], 'de', nome); // D de Dayan
+ 
+ // ex 01 fazendo cópia
+ let a = 'A';
+ let b = a;
+ console.log(a, b); // A A
+
+ // auterando valor
+ a = 'Outra coisa';
+ console.log(a, b); // Outra coisa A
+
+// ex 02
+let c = [1, 2, 3];
+let d = c;
+console.log(c, d); /*
+[ 1, 2, 3 ] [ 1, 2, 3 ]
+nesse caso tanto c quanto d apontam para o mesmo local do dado na memória 
+*/
+
+c.push(4);
+console.log(c, d); /*
+[ 1, 2, 3, 4 ] [ 1, 2, 3, 4 ]
+ao incluir o 4 em c alterei tbm em d
+*/
+
+// porém se altero d tbm afeto c
+d.pop();
+console.log(c, d); // [ 1, 2, 3 ] [ 1, 2, 3 ]
