6e80832694582af3049f1b411d4f4d187ff615fb
Dayan Montiro <dmsmamb@gmail.com>
2021-01-09 19:42:30 -0300

    challenge 04

--- JS-v/aula04/challenge-04.js
+++ JS-v/aula04/challenge-04.js
@@ -108,7 +108,22 @@ mostrar quantos assentos ainda podem ser ocupados, com a frase:
 - Se couber somente mais uma pessoa, mostrar a palavra "pessoa" no retorno
 citado acima, no lugar de "pessoas".
 */
-?
+carro.adicionarPessoas = function(numeroPessoas) {
+    var totalPessoas = carro.quantidadePessoas + numeroPessoas;
+    if(carro.quantidadePessoas === carro.assentos && totalPessoas >= carro.assentos){
+        return 'O carro já esta lotado!';
+    }
+
+    if(totalPessoas > carro.assentos) {
+        var quantasPessoasCabem = carro.assentos - carro.quantidadePessoas;
+        var pluralOuSingular = quantasPessoasCabem === 1 ? 'pessoa' : 'pessoas';
+        return 'Só ' + cabeOuCabem + ' mais ' + quantasPessoasCabem +  pluralOuSingular +  '!';
+    }
+
+    carro.quantidadePessoas += numeroPessoas;
+    return 'Já temos ' + carro.quantidadePessoas + ' pessoas no carro!';
+};
+
 
 /*
 Agora vamos verificar algumas informações do carro. Para as respostas abaixo,
@@ -135,19 +150,19 @@ carro.obterCor(); // verde musgo
 carro.obterMarcaModelo(); // "Esse carro é um Fiat uno"
 
 // Adicione 2 pessoas no carro.
-?
+carro.adicionarPessoas(2); // "Já temos 2 pessoas no carro!"
 
 // Adicione mais 4 pessoas no carro.
-?
+carro.adicionarPessoas(4); // "Só cabem mais 3 pessoas!"
 
 // Faça o carro encher.
-?
+carro.adicionarPessoas(3); // "Já temos 5 pessoas no carro!"
 
 // Tire 4 pessoas do carro.
-?
+carro.adicionarPessoas(-4); // "Já temos 1 pessoas no carro!"
 
 // Adicione 10 pessoas no carro.
-?
+carro.adicionarPessoas(10); // "Só cabem mais 4 pessoas!"
 
 // Quantas pessoas temos no carro?
-?
+carro.quantidadePessoas; // 1
