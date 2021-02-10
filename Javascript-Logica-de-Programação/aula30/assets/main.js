e6755d472efe318449c1c1018074a0005c3790c2
Dayan Monteiro <dmsmamb@gmail.com>
2021-02-10 11:12:24 -0300

    aula 30

--- Javascript-Logica-de-Programação/aula30/assets/main.js
+++ Javascript-Logica-de-Programação/aula30/assets/main.js
@@ -1,3 +1,5 @@
+/* modelo 01 - funciona mas é muito código pode ser bem menor
+
 const h1 = document.querySelector('.container h1');
 const data = new Date();
 h1.innerHTML = data.toString();
@@ -34,15 +36,77 @@ function getDiaSemanaTexto(diaSemana) {
 }
 
 function getNomeMes(numeroMes) {
-    let diaSemanaTexto;
+    let nomeMes;
 
     switch(numeroMes) {
         case 0:
-            diaSemanaTexto = 'Domingo';
-        return diaSemanaTexto; 
-        
-        
+            nomeMes  = 'janeiro';
+        return nomeMes;  
+        case 1:
+            nomeMes  = 'fevereiro';
+        return nomeMes; 
+        case 2:
+            nomeMes  = 'março';
+        return nomeMes; 
+        case 3:
+            nomeMes  = 'abril';
+        return nomeMes; 
+        case 4:
+            nomeMes  = 'maio';
+        return nomeMes; 
+        case 5:
+            nomeMes  = 'junho';
+        return nomeMes; 
+        case 6:
+            nomeMes  = 'julho';
+        return nomeMes;  
+        case 7:
+            nomeMes  = 'agosto';
+        return nomeMes; 
+        case 8:
+            nomeMes  = 'setembro';
+        return nomeMes;
+        case 9:
+            nomeMes  = 'outubro';
+        return nomeMes; 
+        case 10:
+            nomeMes  = 'novembro';
+        return nomeMes;
+        case 11:
+            nomeMes  = 'dezembro';
+        return nomeMes;         
 }
 
+function criaData(data) {
+    const diaSemana = data.datagetDay();
+    const numeroMes = data.getMonth();
 
-h1.innerHTML =  getDiaSemanaTexto(data.getDay());
+    const nomeDia = getDiaSemanaTexto(diaSemana);
+    const nomeMes = getNomeMes(numeroMes);
+
+    return (
+    `${nomeDia}, ${data.getDate()} de ${nomeMes}` + 
+    ` de ${data.getFullYear()} ` +
+    ` $zeroAEsquerda(data.getHours())}:
+    ${zeroAEsqueda(data.getMinutes())}`
+    );
+}
+
+h1.innerHTML =  criaData(data)
+*/
+
+/* modelo 02
+
+const h1 = document.querySelector('.container h1');
+const data = new Date();
+const opcoes = {
+    dateStyle: 'full',
+    timeStyle: 'short'
+};
+
+h1.innerHTML =  data.toLocaleDateString('pt-BR', opcoes); */
+
+// modelo 3
+const h1 = document.querySelector('.container h1');
+const data = new Date();
+h1.innerHTML = data.toLocaleDateString('pt-BR', { dateStyle: 'full', timeStyle: 'short' });
