e4057a8c3bd34dd0716b8f99a8ac9a9be9e67ba7
DayanMonteiro <dmsmamb@gmail.com>
2021-02-23 22:46:07 -0300

    delete em use string

--- JS-FD/aula16/use-strict.js
+++ JS-FD/aula16/use-strict.js
@@ -37,4 +37,31 @@ que o use strict n ermite o uso de variáveis sem o var
         this.age = age;
     }
     console.log(new Person('Dayan', 'MOnteiro', 33));
+
+
+    // usando delete
+    var obj1 = {
+        propriedade1: 'propriedade1',
+        propriedade2: 'propriedade2',
+        propriedade3: 'propriedade3',
+    }
+    console.log(delete obj1.propriedade1, obj1);
+
+    // porem se eu passar uma variavel utilizando var o delete não consegue deleta dentro do use strict
+    (function() {
+        var myVar = 2;
+        var obj2 = {
+            propriedade4: 'propriedade4',
+            propriedade5: 'propriedade5',
+            propriedade6: 'propriedade6',
+        };
+        /* não consigo deletar a variável devido ao user strict, mas propriedades eu consigo
+        console.log(delete myVar);*/
+
+        // ja propriedades consigo deletar, vejamos:
+        console.log(delete obj2.propriedade4, obj2);
+    }());
+
 }());
+
+
