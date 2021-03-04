d79328d5865bca826a06aa30763193fe7ea3a1f0
Dayan Monteiro <dmsmamb@gmail.com>
2021-03-04 09:48:09 -0300

    métodos length, toString, .call(this)

--- JS-FD/aula22/prop-method.js
+++ JS-FD/aula22/prop-method.js
@@ -24,7 +24,7 @@
 
 // ex .toString() atuando em um objeto
     function myFunction1(a, b, c, d) {
-
+        
     }
     var obj = {prop1: 1, prop2: 2};
 
@@ -37,5 +37,36 @@
     }
     ele converte e retorna toda a função como string
     */
+
+    // .call() - ele invoca a função
+    function myFunction2(a, b, c, d, e) {
+        return 'Oi!';
+    }
+    var obj1 = {propriedade1: 1, propriedade2: 2};
+
+    console.log(myFunction2.call()); // Oi! - ate aqui seria a mesmo coisa que invocar apenas o myFunction2, começa a ficar diferente quando invocamos com this
+
+    // .call(this) - quando usado podemos passar quem será o this daquela função
+    function myFunction3(a, b, c, d, e) {
+       console.log(this.name);
+    }
+    // esse obj2 passa a ser o this referenciado em name
+    var obj2 = {
+            name: 'Dayan'
+        };
+
+    var obj3 = {
+        name: 'Monteiro'
+    };
+    
+    var obj4 = {
+        name: 'Santos'
+    };
+
+     myFunction3.call(obj2); // Dayan -- sendo assim o call(this) permite passar por parametro quem será aquela função
+
+    myFunction3.call(obj3); // Monteiro - o interessante é que posso passar vários objetos desde que os referencie como name consigo atribuir e invocar através do this vários deles
+
+    myFunction3.call(obj4); // Santos - e assim segue com quantos mais objetos eu atribuir ao name referenciado pelo this
 
 })();
