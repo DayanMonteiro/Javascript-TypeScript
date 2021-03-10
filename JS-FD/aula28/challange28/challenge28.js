9157595c74fbdd681a677f8050bf9feef16c71df
Dayan Monteiro <dmsmamb@gmail.com>
2021-03-10 16:16:33 -0300

    introduzindo API de consulta de CEP

---
+++ JS-FD/aula28/challange28/challenge28.js
@@ -0,0 +1,142 @@
+(function(){
+  'use strict';
+/*
+OBS USAR http://apps.widenet.com.br/busca-cep/api-de-consulta
+Guarde esse link para usar na hora da solução do desafio 28:
+
+http://apps.widenet.com.br/busca-cep/api-de-consulta
+
+Detalhe: esse problema veio à tona, pois um aluno teve uma dúvida por conta do problema e abriu uma Issue no GitHub.
+
+Então se você quiser saber mais detalhes sobre o problema, e mais informações sobre o uso dessa nova API, dá uma olhadinha na issue abaixo, fechado? =)
+
+https://github.com/da2k/curso-javascript-ninja/issues/2048
+
+O DESAFIO COMEÇA AQUI
+
+  No HTML:
+  - Crie um formulário com um input de texto que receberá um CEP e um botão
+  de submit;
+  - Crie uma estrutura HTML para receber informações de endereço:
+  "Logradouro, Bairro, Estado, Cidade e CEP." Essas informações serão
+  preenchidas com os dados da requisição feita no JS.
+  - Crie uma área que receberá mensagens com o status da requisição:
+  "Carregando, sucesso ou erro."
+  No JS:
+  - O CEP pode ser entrado pelo usuário com qualquer tipo de caractere, mas
+  deve ser limpo e enviado somente os números para a requisição abaixo;
+  - Ao submeter esse formulário, deve ser feito um request Ajax para a URL:
+  "https://viacep.com.br/ws/[CEP]/json/", onde [CEP] será o CEP passado
+  no input criado no HTML;
+  - Essa requisição trará dados de um CEP em JSON. Preencha campos na tela
+  com os dados recebidos.
+  - Enquanto os dados são buscados, na área de mensagens de status, deve mostrar
+  a mensagem: "Buscando informações para o CEP [CEP]..."
+  - Se não houver dados para o CEP entrado, mostrar a mensagem:
+  "Não encontramos o endereço para o CEP [CEP]."
+  - Se houver endereço para o CEP digitado, mostre a mensagem:
+  "Endereço referente ao CEP [CEP]:"
+  - Utilize a lib DOM criada anteriormente para facilitar a manipulação e
+  adicionar as informações em tela.
+  */
+ 
+    function DOM(elements){
+        this.element = document.querySelectorAll(elements);
+      }
+      
+      DOM.prototype.on = function on(eventType, callback) {
+        Array.prototype.forEach.call(this.element, function(element){
+          element.addEventListener(eventType, callback, false);
+        });
+      };
+      
+      DOM.prototype.off = function off(eventType, callback) {
+        Array.prototype.forEach.call(this.element, function(element){
+          element.removeEventListener(eventType, callback, false);
+        });
+      };
+      
+      DOM.prototype.get = function get() {
+        return this.element;
+      };
+    
+      DOM.prototype.forEach = function forEach() {
+        return Array.prototype.forEach.apply(this.element, arguments);
+      };
+    
+      DOM.prototype.map = function map() {
+        return Array.prototype.map.apply(this.element, arguments);
+      };
+    
+      DOM.prototype.filter = function filter() {
+        return Array.prototype.filter.apply(this.element, arguments);
+      };
+    
+      DOM.prototype.reduce = function reduce() {
+        return Array.prototype.reduce.apply(this.element, arguments);
+      };
+    
+      DOM.prototype.reduceRight = function reduceRight() {
+        return Array.prototype.reduceRight.apply(this.element, arguments);
+      };
+    
+      DOM.prototype.every = function every() {
+        return Array.prototype.every.apply(this.element, arguments);
+      };
+    
+      DOM.prototype.some = function some() {
+        return Array.prototype.some.apply(this.element, arguments);
+      }; 
+    
+    DOM.prototype.isArray = function isArray(parametro){
+      return Object.prototype.toString.call(parametro) === '[object Array]';
+    };
+    
+    DOM.prototype.isObject = function isObject(parametro){
+      return Object.prototype.toString.call(parametro) === '[object Object]';
+    };
+    
+    DOM.prototype.isFunction = function isFunction(parametro){
+      return Object.prototype.toString.call(parametro) === '[object Function]';
+    };
+  
+    
+    DOM.prototype.isNumber = function isNumber(parametro){
+      return Object.prototype.toString.call(parametro) === '[object Number]';
+    };
+    
+    DOM.prototype.isString = function isString(parametro){
+      return Object.prototype.toString.call(parametro) === '[object String]';
+    };
+    
+    DOM.prototype.isBoolean = function isBoolean(parametro){
+      return Object.prototype.toString.call(parametro) === '[object Boolean]';
+    };
+    
+    DOM.prototype.isNull = function isNull(parametro){
+      return Object.prototype.toString.call(parametro) === '[object Null]'
+      || Object.prototype.toString.call(parametro) === '[object Undefined]';
+    };
+
+    
+    var $formCEP = new DOM('[data-js="form-cep"]');
+    var $inputCEP = new DOM('[data-js="input-cep"]');
+    var ajax = new XMLHttpRequest();
+    $formCEP.on('submit', handleSubmitFormCEP);
+     
+    
+    function handleSubmitFormCEP(event) {
+      event.preventDefault();
+      console.log($inputCEP.get()[0].value);
+      var url = 'https://viacep.com.br/ws/[CEP]/json'.replace('[CEP]', $inputCEP.get()[0].value);
+     ajax.open('GET', url);
+     ajax.send();
+     ajax.addEventListener('readystatechange', handleReadyStateChange);
+    }
+
+    function handleReadyStateChange() {
+      if(ajax.readyState === 4 && ajax.status === 200);
+      console.log('Popular formulário', ajax.responseText);
+    }
+
+    }());
