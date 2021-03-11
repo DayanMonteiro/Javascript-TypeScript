867609c7ea0cb8907652f9ffbc97b4dc11abc464
Dayan Monteiro <dmsmamb@gmail.com>
2021-03-11 17:30:27 -0300

    refatorando

--- JS-FD/aula28/challange28/challenge28.js
+++ JS-FD/aula28/challange28/challenge28.js
@@ -121,6 +121,12 @@ O DESAFIO COMEÇA AQUI
     
     var $formCEP = new DOM('[data-js="form-cep"]');
     var $inputCEP = new DOM('[data-js="input-cep"]');
+    var $logradouro = new DOM('[data-js="logradouro"]');
+    var $bairro = new DOM('[data-js="bairro"]');
+    var $estado = new DOM('[data-js="estado"]');
+    var $cidade = new DOM('[data-js="cidade"]');
+    var $cep = new DOM('[data-js="cep"]');
+    var $status = new DOM('[data-js="status"]');
     var ajax = new XMLHttpRequest();
     $formCEP.on('submit', handleSubmitFormCEP);
      
@@ -131,16 +137,23 @@ O DESAFIO COMEÇA AQUI
       var url = getUrl();
      ajax.open('GET', url);
      ajax.send();
+     getMessage('loading');
      ajax.addEventListener('readystatechange', handleReadyStateChange);
     }
 
     function getUrl() {
-      return 'https://viacep.com.br/ws/[CEP]/json'.replace('[CEP]', $inputCEP.get()[0].value.replace(/\D/g, ''));
+      return replaceCEP('https://viacep.com.br/ws/[CEP]/json');
+    }
+
+    function clearCEP() {
+      return $inputCEP.get()[0].value.replace(/\D/g, '');
     }
 
     function handleReadyStateChange() {
-      if(isRequestOk()) 
-      fillCEPFields();
+      if(isRequestOk()) {
+        getMessage('ok');
+        fillCEPFields();
+      }
     }
 
     function isRequestOk() {
@@ -149,19 +162,26 @@ O DESAFIO COMEÇA AQUI
 
     function fillCEPFields() {
       var data = parseData();
-      if(!data)
-        return console.log('DATA ERROR', data);
-      console.log('DATA', data);
-      var $logradouro = new DOM('[data-js="logradouro"]');
-      var $bairro = new DOM('[data-js="bairro"]');
-      var $estado = new DOM('[data-js="estado"]');
-      var $cidade = new DOM('[data-js="cidade"]');
-      var $cep = new DOM('[data-js="cep"]');
+      if(!data) {
+        getMessage('error');
+        data = clearData();
+      }
+
      $logradouro.get()[0].textContent = data.logradouro;
      $bairro.get()[0].textContent = data.bairro;
      $estado.get()[0].textContent = data.uf;
      $cidade.get()[0].textContent = data.localidade;
      $cep.get()[0].textContent = data.cep;
+    }
+
+    function clearData() {
+      return {
+        logradouro: '-',
+        bairro: '-',
+        uf: '-',
+        localidade: '-',
+        cep: '-'
+      }
     }
 
     function parseData() {
@@ -177,11 +197,15 @@ O DESAFIO COMEÇA AQUI
 
     function getMessage(type) {
       var messages = {
-        loading: 'Buscando informações para o CEP [CEP]...',
-        ok: 'Endereço referente ao CEP [CEP]:',
-        error: 'Não encontramos o endereço para o CEP [CEP].'
-      };
-      return messages[type];
+        loading: replaceCEP('Buscando informações para o CEP [CEP]...'),
+        ok: replaceCEP('Endereço referente ao CEP [CEP]:'),
+        error: replaceCEP('Não encontramos o endereço para o CEP [CEP].')
+      };    
+      $status.get()[0].textContent = messages[type];
+    }
+
+    function replaceCEP(message) {
+      return message.replace('[CEP]', clearCEP());
     }
 
     }());
