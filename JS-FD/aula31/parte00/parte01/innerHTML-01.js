e2bff1e42b36fdf9fd79530855b3627ec4faa3bb
Dayan Monteiro <dmsmamb@gmail.com>
2021-03-16 16:58:08 -0300

    innerHTML

---
+++ JS-FD/aula31/parte00/parte01/innerHTML-01.js
@@ -0,0 +1,11 @@
+// innerHTML parte 02 - problemas de segurança
+(function(){
+    'use strict';
+   
+    var $div = document.querySelector('[data-js="main"]');
+    var $textarea = document.querySelector('[data-js="textarea"]');
+    var $form = document.querySelector('[data-js="form"]');
+   
+   
+   }());
+   
