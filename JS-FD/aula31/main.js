d7fe035e593a34ba40b6c945727b7c5d68dc50e2
Dayan Monteiro <dmsmamb@gmail.com>
2021-03-16 16:02:55 -0300

    inneinnerHTML parte 01

--- JS-FD/aula31/main.js
+++ JS-FD/aula31/main.js
@@ -1,9 +1,16 @@
+// innerHTML parte 01
 (function(){
  'use strict';
 
  var $div = document.querySelector('[data-js="main"]');
- console.log($div);
+ console.log($div.innerHTML); // getter
+// $div.innerHTML = '<h2>Título 2</h2>'; // setter
+var $h2 = document.createElement('h2');
+$h2.textContent = 'Título 2';
+$div.appendChild($h2);
 
+// passando por concatenação
+$div.innerHTML += '<h3>Título 3</h3>';
 
 
 }());
